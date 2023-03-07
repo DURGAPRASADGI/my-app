734703d0561df1f230a4ff231e74f339aa178e6c
DURGAPRASADGI <116330067+DURGAPRASADGI@users.noreply.github.com>
2023-03-07 15:22:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
         a="\images\a1.jpg"
         b="\images\a10.jpg"
         c="\images\a3.png"
-        n1="venu"
+        n1="priya"
         n2="Ethan"
         n3="Amelia"
         p1="Data Scientist"
